0e177395f73820d2bd74bdf81650344e4cc9db95
Jingbei Li <i@jingbei.li>
2016-07-05 17:21:56 +0800

    python3 migration

--- download-icons.py
+++ download-icons.py
@@ -1,12 +1,13 @@
 import json
-import urllib
+import urllib.request
 import os
 icons=json.load(open('index-icon.json'))
 for a in icons['fix']:
-    img=urllib.urlopen(a['icon']).read()
+    img=urllib.request.urlopen(a['icon']).read()
     filename='gif/'+a['title']+'.gif'
     if os.path.exists(filename):
         continue
+    print(a['title'])
     f=open(filename,'wb')
     f.write(img)
     f.close()

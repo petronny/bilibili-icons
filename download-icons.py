f39707ca397ff92cd02fe8f313558a42c39809b4
Jingbei Li <i@jingbei.li>
2016-07-09 10:30:08 +0800

    fix logical mistake

--- download-icons.py
+++ download-icons.py
@@ -3,11 +3,11 @@ import urllib.request
 import os
 icons=json.load(open('index-icon.json'))
 for a in icons['fix']:
-    img=urllib.request.urlopen(a['icon']).read()
     filename='gif/'+a['title']+'.gif'
     if os.path.exists(filename):
         continue
     print(a['title'])
     f=open(filename,'wb')
+    img=urllib.request.urlopen(a['icon']).read()
     f.write(img)
     f.close()

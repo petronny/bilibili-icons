5afced1c55019ae4fca7e8a2b18cacf81b20da4d
Jingbei Li <i@jingbei.li>
2016-09-04 21:53:49 +0800

    enable auto-update

--- bilibili-icons.sh
+++ bilibili-icons.sh
@@ -2,3 +2,6 @@ wget http://www.bilibili.com/index/index-icon.json -O index-icon.json.gz
 gzip -d -f index-icon.json.gz
 [ ! -d gif ] && mkdir gif
 python download-icons.py
+git add .
+git commit -m "[$(LANG=C date)]auto update"
+git push

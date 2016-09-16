4e78381ccd46fe8513f760d286ab0e68cc9c3474
calil <calil@jingbei.li>
2016-09-16 15:12:08 +0800

    [Fri Sep 16 15:12:08 CST 2016]auto update

--- download-icons.py
+++ download-icons.py
@@ -4,8 +4,8 @@ import os
 icons=json.load(open('index-icon.json'))
 for a in icons['fix']:
     filename='gif/'+a['title']+'.gif'
-    if os.path.exists(filename):
-        continue
+    #if os.path.exists(filename):
+    #    continue
     print(a['title'])
     f=open(filename,'wb')
     img=urllib.request.urlopen(a['icon']).read()

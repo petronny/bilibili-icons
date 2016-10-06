4da395fc5fbf7258b3ae1e62ad59ac01097e10ad
Jingbei Li <i@jingbei.li>
2016-10-06 22:23:24 +0800

    made index.json more readable

--- calil.sh
+++ calil.sh
@@ -3,6 +3,7 @@ cd `dirname $0`
 git reset --hard
 git pull
 sh bilibili-icons.sh
+python parse-index.py
 js-beautify -i index-icon.json -o index-icon.json
 git add .
 git commit -m "[$(LANG=C date)]auto update"
---
+++ parse-index.py
@@ -0,0 +1,9 @@
+import json
+import urllib.parse
+icons=json.load(open('index-icon.json'))
+for a in icons['fix']:
+    for i in range(0,len(a['links'])):
+        a['links'][i]=urllib.parse.unquote(a['links'][i])
+f=open('index-icon.json','w')
+json.dump(icons,f,ensure_ascii=False)
+f.close()

313363a5275c25c80b5e5ce77e2747e9c28c9867
Jingbei Li <i@jingbei.li>
2017-04-12 13:04:54 +0800

    don't need gz anymore

--- bilibili-icons.sh
+++ bilibili-icons.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-wget http://www.bilibili.com/index/index-icon.json -O index-icon.json.gz
-gzip -d -f index-icon.json.gz
+wget http://www.bilibili.com/index/index-icon.json -O index-icon.json
 [ ! -d gif ] && mkdir gif
 python download-icons.py

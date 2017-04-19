f660f1ff1143b1a917984fc7ee8bb9cfbc1a5e0f
Jingbei Li <i@jingbei.li>
2017-04-19 20:18:34 +0800

    added log.txt

--- calil.sh
+++ calil.sh
@@ -6,7 +6,7 @@ git pull
 sh bilibili-icons.sh
 python parse-index.py
 js-beautify -i index-icon.json -o index-icon.json
+) > log.txt
 git add .
 git commit -m "[$(LANG=C date)]auto update"
 git push
-) > log.txt

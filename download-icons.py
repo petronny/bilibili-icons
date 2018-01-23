1d8a7e39b00bd22e9f36ca9f269a26a169b5b8cf
Jingbei Li <i@jingbei.li>
2018-01-23 16:50:10 +0800

    added timeout in download-icons.py

--- download-icons.py
+++ download-icons.py
@@ -1,7 +1,8 @@
+#!/bin/python3
 import json
 import urllib.request
 import os
-import time
+from socket import timeout
 icons=json.load(open('index-icon.json'))
 for a in icons['fix']:
     filename='gif/'+a['title']+'.gif'
@@ -11,8 +12,13 @@ for a in icons['fix']:
     if a['icon'][0:2]=='//':
         a['icon']='http:'+a['icon']
     a['icon']=a['icon'].replace('http:////','http://')
-    img=urllib.request.urlopen(a['icon']).read()
+    flag=False
+    while not flag:
+        try:
+            img=urllib.request.urlopen(a['icon'],timeout=3).read()
+            flag=True
+        except timeout:
+            flag=False
     f=open(filename,'wb')
     f.write(img)
     f.close()
-    time.sleep(0.5)
